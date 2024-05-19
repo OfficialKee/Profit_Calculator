7c5ef0edc0c6640c7e51297312746545b6c904f9
Kee Maloney <keemaloney@Kees-MBP.attlocal.net>
2024-05-19 12:48:29 -0400

    refactor in to functions

--- main.go
+++ main.go
@@ -5,21 +5,39 @@ import (
 )
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
-	
-	fmt.Println("What is your revenue? Enter below: ")
-	fmt.Scan(&revenue)
-	fmt.Println("What are your expenses? Enter below: ")
-	fmt.Scan(&expenses)
-	fmt.Println("What is your annual tax rate? Enter below: ")
-	fmt.Scan(&taxRate)
 
-	var earningsBeforeTax float64 = revenue - expenses
-	var profit float64 = earningsBeforeTax - (earningsBeforeTax * (taxRate/100))
-	var ratio float64 = earningsBeforeTax / profit
+	
+	revenue := getRevenue()
+	expenses := getExpenses()
+	taxRate := getTaxRate()
+
+	earningsBeforeTax,profit,ratio := calculateEarnings(revenue,expenses,taxRate)
 
 	fmt.Printf("Earnings Before Tax: %v \nProfit: %v\nRatio: %v", earningsBeforeTax, profit, ratio)
 
+}
+
+func getRevenue()float64{
+	var revenue float64
+	fmt.Println("What is your revenue? Enter below: ")
+	fmt.Scan(&revenue)
+	return revenue
+}
+func getExpenses()float64{
+	var expenses float64
+	fmt.Println("What are your expenses? Enter below: ")
+	fmt.Scan(&expenses)
+	return expenses
+}
+func getTaxRate()float64{
+	var taxRate float64
+	fmt.Println("What is your taxRate? Enter below: ")
+	fmt.Scan(&taxRate)
+	return taxRate
+}
+func calculateEarnings(revenue,expenses,taxRate float64)(float64,float64,float64){
+	earningsBeforeTax := revenue - expenses
+	profit := earningsBeforeTax - (earningsBeforeTax * (taxRate/100))
+	ratio  := earningsBeforeTax / profit
+	return earningsBeforeTax,profit,ratio
 }
